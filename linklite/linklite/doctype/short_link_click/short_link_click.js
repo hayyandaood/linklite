6811282b447dabd868a2a8253a23a7df2a6e38c1
hayyandaood <hayyandaood90@gmail.com>
2025-07-22 21:28:40 +0000

    feat: track clicks

---
+++ linklite/linklite/doctype/short_link_click/short_link_click.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Hayyan Daood and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Short Link Click", {
+// 	refresh(frm) {
+
+// 	},
+// });
