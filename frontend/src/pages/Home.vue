7d495fcddc2739d829799e70c869cf6380c150c3
hayyandaood <hayyandaood90@gmail.com>
2025-07-24 11:41:46 +0000

    feat: keyboard shortcut for creating links

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -2,13 +2,18 @@
   <div class='m-5'>
     <div class="flex items-baseline justify-between mb-4">
         <h2 class="text-gray-900 font-semibold">Links</h2>
-        <Button variant="solid" @click="createDialogShown = true">Create</Button>
+        <Button variant="solid" @click="createDialogShown = true">
+			Create Link
+			<template #suffix>
+				<span class=" font-mono bg-white/80 text-gray-700 rounded-sm px-1">C</span>            
+            </template>
+		</Button>
     </div>
     <ListView v-if="links.data"
     rowKey="name"
     :columns="[{
         label: 'Short Link',
-        key: 'name',
+        key: 'short_link',
         width: 0.4
     },
     {
@@ -31,11 +36,17 @@
         actions: [{
             label: 'Create',
             variant: 'solid',
-            onClick(close){
-              links.insert.submit({
-                ...newLink
-              })
-              close();
+            async onClick(close) {
+                console.log('About to submit:', { ...newLink })
+                await links.insert.submit({ ...newLink })
+                console.log('Success! Closing dialog.')
+                // Reset form
+                newLink.short_link = ''
+                newLink.description = ''
+                newLink.destination_url = ''
+                // Reload the list to show new data
+                links.reload()
+                close()
             }
         }]
         }" v-model="createDialogShown">
@@ -48,57 +59,41 @@
               />
               <FormControl
                   type="text"
-                  label="destination_url"
+                  label="Destination URL"
                   v-model="newLink.destination_url"
               />
               <FormControl
                   type="textarea"
-                  label="description"
+                  label="Description"
                   v-model="newLink.description"
               />
           </form>
-          <ErrorMessage class="mt-2" :message="links.insert.error"
-          />
-        </template>
-        <template #actions>
-
+          <ErrorMessage class="mt-2" :message="links.insert.error" />
         </template>
     </Dialog>
-    
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue"
-import { ListView, Dialog, FormControl, ErrorMessage } from "frappe-ui"
-//import { createListResource } from 'frappe-ui'
-import { useList } from "frappe-ui" //working 
-import { reactive } from 'vue'
+import { ref, reactive } from "vue"
+import { onKeyStroke } from "@vueuse/core";
+import { ListView, Dialog, FormControl, ErrorMessage, Button } from "frappe-ui"
+import { useList } from "frappe-ui"
+
 const createDialogShown = ref(false)
-const newLink = reactive ({
-    name:'',
+
+const newLink = reactive({
+    short_link: '',
     description: '',
-    destination_url: ''
+    destination_url: '',
 })
+
+onKeyStroke(["c", "C"], () => {
+	createDialogShown.value = true;
+});
 const links = useList({
   doctype: 'Short Link',
-  fields: ['name','destination_url','description'],
+  fields: ['short_link','destination_url','description'],
   orderBy: 'creation desc' 
 })
 </script>
-
-
-<!-- 
-            <form>
-                <FormControl
-                    type="text"
-                    label="Short"
-                    v-model="newLink.name"
-                />
-            </form>
-
-            const newLink = reactive ({
-    name:'',
-    description: '',
-    destination_url: ''
-}) -->
