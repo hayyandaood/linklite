14482aac7c94c747bc33feab19fc05d6e34cec33
hayyandaood <hayyandaood90@gmail.com>
2025-07-27 12:49:02 +0000

    fix: update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
 			jinjaBootData: true,
 			lucideIcons: true,
 			buildConfig: {
-				indexHtmlPath: "../<app-name>/www/frontend.html",
+				indexHtmlPath: "/linklite/www/links.html",
 				emptyOutDir: true,
 				sourcemap: true,
 			},
@@ -20,7 +20,7 @@ export default defineConfig({
 	],
 	build: {
 		chunkSizeWarningLimit: 1500,
-		outDir: "../<app-name>/public/frontend",
+		outDir: "../linklite/public/frontend",
 		emptyOutDir: true,
 		target: "es2015",
 		sourcemap: true,
